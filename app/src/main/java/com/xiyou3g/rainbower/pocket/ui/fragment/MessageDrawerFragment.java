a1432682e4d56be3a6ee1664ad9f7883c5cc21d3
cuijiaming@guazi.com <cuijiaming@guazi.com>
2018-02-26 16:42:57 +0800

    modify card fragment

---
+++ app/src/main/java/com/xiyou3g/rainbower/pocket/ui/fragment/MessageDrawerFragment.java
@@ -0,0 +1,9 @@
+package com.xiyou3g.rainbower.pocket.ui.fragment;
+
+/**
+ * Created by jegaming on 2018/2/26.
+ */
+
+public class MessageDrawerFragment {
+
+}
---
+++ app/src/main/java/com/xiyou3g/rainbower/pocket/ui/fragment/MineDrawerFragment.java
@@ -0,0 +1,11 @@
+package com.xiyou3g.rainbower.pocket.ui.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by jegaming on 2018/2/26.
+ */
+
+public class MineDrawerFragment extends Fragment {
+
+}
